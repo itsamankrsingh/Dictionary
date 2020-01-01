7d1cbce455d7122d664342e294e7b4cfc2a2f0cf
Aman Kumar Singh <itsamanrsingh@gmail.com>
2020-01-01 20:02:09 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+from difflib import get_close_matches
+
+
+data=json.load(open("data.json"))
+
+def translate(word):
+    word=word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word,data.keys()))>0:
+        yn=input("Did you mean %s instead? Enter Y if yes or enter N if no." %get_close_matches(word,data.keys())[0])
+        yn=yn.upper()
+        if yn=="Y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif yn=="N":
+            return "The word doesn't exit.Try another word.Thank You"
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exit.Try another word.Thank You"
+
+word=input("Enter word you want to search:")
+output=translate(word)
+
+
+if type(output)==list:
+    for item in output:
+        print(item)
+else:
+    print(output)
+
+
